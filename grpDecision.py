af2174c41d875bd4edd012d14bb1cb2c16d2aff0
kimathinjoki <dfkimathinjoki@gmail.com>
2024-08-20 05:59:35 -0500

    first commit

---
+++ grpDecision.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/group_decision', methods=['POST'])
+
+
+def group_decision():
+    data = request.get_json("data", [])
+
+    results = {}
+
+    for column, values in data.items():
+        num_val = [float(i) for i in values if isinstance(i, (int, float))]
+        if num_val:
+            avg = np.mean(num_val)
+            results[column] = avg
+    return jsonify(results)
+
+if __name__ == '__main__':
