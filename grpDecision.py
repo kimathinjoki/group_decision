e80b2df6efbd6fdee25449717509cbab86a8eedc
kimathinjoki <dfkimathinjoki@gmail.com>
2024-08-20 11:06:22 -0500

    added port binding

--- grpDecision.py
+++ grpDecision.py
@@ -20,4 +20,5 @@ def group_decision():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
